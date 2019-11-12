5c4fdbb696a4dd97ff42a417b340a7b0aa12c39a
Sam Jirovec <jirov004@umn.edu>
2019-11-12 15:24:07 -0600

    almost finished

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, render_template
+from flask import Flask, request, Response, render_template, json, jsonify, flash
 import requests
 import re
 import itertools
@@ -6,16 +6,21 @@ from flask_wtf.csrf import CSRFProtect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+#Key to use on webster api
+key = '325142d6-6793-4d6f-8f67-8b23d76755e0'
+
+
+# Select field added in the template, validation inside of the function itself, not the form.
 class WordForm(FlaskForm):
     avail_letters = StringField("Letters")
     submit = SubmitField("Go")
-
 
 csrf = CSRFProtect()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "row the boat"
 csrf.init_app(app)
 
+@app.route('/')
 @app.route('/index')
 def index():
     form = WordForm()
@@ -24,6 +29,7 @@ def index():
 
 @app.route('/words', methods=['POST','GET'])
 def letters_2_words():
+
 
     form = WordForm()
     if form.validate_on_submit():
@@ -43,6 +49,7 @@ def letters_2_words():
     with open('sowpods.txt') as f:
         good_words = set(x.strip().lower() for x in f.readlines())
 
+    #build the set and depending on the parameters passed in via the form, utilize word length or pattern.
     word_set = set()
     if letters != '':
         for l in range(3,len(letters)+1):
@@ -51,7 +58,6 @@ def letters_2_words():
                 if w in good_words:
                     if word_length != 'default':
                         if int(word_length) == len(w) and re.fullmatch(pattern,w) != None and pattern != '':
-                            print(w)
                             word_set.add(w)
                         elif int(word_length) == len(w) and pattern == '':
                             word_set.add(w)
@@ -60,11 +66,11 @@ def letters_2_words():
                             word_set.add(w)
                         elif pattern == '':
                             word_set.add(w)
+    #Same Pattern, just go through all words if none are specified.
     else:
         for w in good_words:
              if word_length != 'default':
                 if int(word_length) == len(w) and re.fullmatch(pattern,w) != None and pattern != '':
-                    print(w)
                     word_set.add(w)
                 elif int(word_length) == len(w) and pattern == '':
                     word_set.add(w)
@@ -75,15 +81,26 @@ def letters_2_words():
                     word_set.add(w)
 
 
-    #sorting the word list 
+    #sorting the word list first by alphabet and then by length.
     wordlist = list(word_set)
-    wordlist.sort()
+    wordlist = sorted(wordlist)
+    wordlist = sorted(wordlist,key=len,reverse=True)
 
     return render_template('wordlist.html',
-        wordlist=sorted(word_set, key=len, reverse=True),
+        wordlist=wordlist,
         name="CS4131")
 
 
+#New Route to make requests from the flask app, this hides the api key from the users who can see the Javascript.
+@app.route('/def/<word>', methods=['GET'])
+def getDef(word):
+    resp = requests.get("https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=" + key)
+    data = resp.json()
+    if type(data[0]) != dict:
+        return jsonify('No Def. Found')
+    else:
+        alt_def = data[0]['shortdef']
+        return jsonify(alt_def)
 
 
 @app.route('/proxy')
--- static/script.js
+++ static/script.js
@@ -1,21 +1,25 @@
 
-var defintion_box = document.getElementById('definition')
+var defintion_box = document.getElementById("definition");
+
 
 async function getDef(word) {
-    // TODO: REMOVE THE API KEY FROM THE CALL HERE 
-    // https://www.dictionaryapi.com/api/v3/references/collegiate/json/test?key=325142d6-6793-4d6f-8f67-8b23d76755e0
-    // https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key
-    let resp = await fetch("https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=325142d6-6793-4d6f-8f67-8b23d76755e0");
-    let dict_resp = await resp.json();
-    let definition = dict_resp[0]['def'];
-    console.log(definition);
-    document.getElementById('definition').innerHTML = "<h3>" + word + "</h3><br>" + definition;
-    document.getElementById('definition').classList.toggle("visible");
-
+    var close_message = "<button class='exit' onclick='goInvis()'> Click Here to Close </button>";
+    var host = window.location.hostname;
+    console.log(host);
+    var resp = await fetch('/def/' + word);
+    var def = await resp.json();
+    console.log(def);
+    if(def.length != 0){
+        document.getElementById('definition').innerHTML = "<b>" + word + "</b> : " + def  + "<br>" + close_message;
+    }
+    else {
+        document.getElementById('definition').innerHTML = "<b>" + word + "</b> : " + "No Def Available" + "<br>" + close_message;
+    }
+    document.getElementById('definition').style.display = "block";
 }
 
-window.onclick = function (event) {
-    if (event.target == defintion_box) {
-        defintion.style.visibility = "none";
-    }
+
+function goInvis() {
+    document.getElementById('definition').style.display = "none";
+    console.log("HERE")
 }
